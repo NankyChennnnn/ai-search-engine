1aa1fde606772638be428bb464f643b7552ce047
bibberzz <zhouqiang.dev@outlook.com>
2026-04-07 14:36:55 +0800

    Update test code.

--- online/server/main.cc
+++ online/server/main.cc
@@ -1,8 +1,33 @@
 #include "SearchEngineServer.h"
+#include <algorithm>
+#include <stdexcept>
 
-int main()
+using std::cout;
+using std::endl;
+
+int main(int args, char *argv[])
 {
-    SearchEngineServer server("helo");
+    string word;
+    if (argv) word = "helo";
+    else word = argv[1];
+
+    static string uc;
+    bool hasNonAlpha = std::any_of(word.begin(), word.end(), [](unsigned char c) {
+        uc = c;
+        return !std::isalpha(c);
+    });
+
+    if (hasNonAlpha)
+    {
+        throw std::runtime_error("[ERROR] Invalid input letter \"" + uc + "\".");
+    }
+
+    std::transform(word.begin(), word.end(), word.begin(), [](unsigned char c) {
+        return std::tolower(c);
+    });
+    cout << "[INFO] Input word \"" << word << "\"." << endl;
+
+    SearchEngineServer server(word);
     server.start();
     return 0;
 }
